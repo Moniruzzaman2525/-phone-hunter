a0595d1c196cd0f333561a1962891b34d89b70ec
unknown <md616264@gmail.com>
2022-03-01 22:28:19 +0600

    add again

--- js/js.js
+++ js/js.js
@@ -1,8 +1,10 @@
 const container = document.getElementById('main');
 const phoneDetails = document.getElementById('mobile-details');
-const inputButton = document.getElementById('input-button').addEventListener('click', phone => {
+
+const inputButton = document.getElementById('input-button').addEventListener('click', () => {
     const input = document.getElementById('input-field');
     const error = document.getElementById('error');
+
     const inputField = input.value;
     // const phone = ['iphone' && 'samsung']
 
@@ -19,6 +21,7 @@ const inputButton = document.getElementById('input-button').addEventListener('cl
         fetch(`https://openapi.programming-hero.com/api/phones?search=${input.value}`)
             .then(resp => resp.json())
             .then(data => phoneDisplay(data.data))
+        input.value = '';
     }
 
 
@@ -26,30 +29,30 @@ const inputButton = document.getElementById('input-button').addEventListener('cl
 
 const phoneDisplay = (phones) => {
     if (phones.length === 0) {
-        error.innerText = "Please enter a phone name";
+        error.innerText = "No phone found";
         container.innerHTML = '';
         phoneDetails.innerHTML = '';
     }
     const first20 = phones.slice(0, 20);
-    for (const phone of first20) {
+    first20.forEach(phone => {
         error.innerText = '';
         // console.log(phone)
         const div = document.createElement('div');
         phoneDetails.innerHTML = '';
         div.innerHTML = `
-        <div class="col">
+        <div class="col shadow-lg p-3 mb-5 bg-body rounded">
             <div class="card">
                 <img src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">${phone.phone_name}</h5>
-                <p class="card-text">${phone.brand}</p>
+                <h5 class="card-title text-success">${phone.phone_name}</h5>
+                <p class="card-text text-info">${phone.brand}</p>
                 <button class="btn btn-primary" onclick="seeDetails('${phone.slug}')">See Details</button>
                 </div>
             </div>
         </div>
         `;
         container.appendChild(div);
-    }
+    })
 }
 
 const seeDetails = (phone) => {
@@ -59,32 +62,34 @@ const seeDetails = (phone) => {
         .then(data => {
             const phoneInfo = data.data;
             // const phoneDetails = document.getElementById('mobile-details');
-            console.log(phoneInfo)
-            console.log(phoneInfo?.releaseDate)
+            console.log(phoneInfo?.name)
             const div = document.createElement('div');
-            main.innerHTML = '';
+            // main.innerHTML = '';
+            phoneDetails.innerHTML = '';
             div.innerHTML = `
-                <div class="card mx-auto" style="width: 20rem;">
-                    <img src="${phoneInfo.image}" class="card-img-top" alt="...">
-                    <div class="card-body text-start">
-                        <h2 class="text-center text-success">${phoneInfo.brand}</h2>
-                        <h2 class="text-center text-success">${phoneInfo.name}</h2>
-                        <div class="card-text">
-                        <h5>First Release: ${phoneInfo.releaseDate ? phoneInfo.releaseDate : "No release date found"}</h5>
-                        <h2>Connectivity</h2>
-                        <p>WLAN: ${phoneInfo.others.WLAN}</p>
-                        <p>Bluetooth: ${phoneInfo.others.Bluetooth}</p>
-                        <p>GPS: ${phoneInfo.others.GPS}</p>
-                        <p>Radio: ${phoneInfo.others.Radio}</p>
-                        <p>USB: ${phoneInfo.others.USB}</p>
-                        <p>NFC: ${phoneInfo.others.NFC}</p>
-                        <p>WLAN: ${phoneInfo.name}</p>
-                        <h2>Performance</h2>
-                        <p>Chipset: ${phoneInfo.mainFeatures.chipSet}</p>
-                        <p>Display: ${phoneInfo.mainFeatures.displaySize}</p>
-                        <p>MicroSD Slot: ${phoneInfo.mainFeatures.storage}</p>
-                        <p>Chipset: ${phoneInfo.mainFeatures.sensors}</p>
-                    </div>
+                <div class="card mx-auto" style="width: 13rem;">
+                    <img width="50px" src="${phoneInfo.image}" class="card-img-top" alt="...">
+                    <div>
+                        <div class="card-body text-start">
+                            <h4 class="text-center text-success">${phoneInfo.name}</h4>
+                            <h2 class="text-center text-info">${phoneInfo.brand}</h2>
+                            <div class="card-text">
+                            <h5>First Release: ${phoneInfo?.releaseDate ? phoneInfo.releaseDate : "No date found"}</h5>
+                            <h2 class="text-primary">Performance:</h2>
+                            <p>Chipset: ${phoneInfo?.chipSet ? phoneInfo.chipSet : "No date found"}</p>
+                            <p>Display: ${phoneInfo?.displaySize ? phoneInfo.displaySize : "No date found"}</p>
+                            <p>MicroSD Slot: ${phoneInfo?.mainFeatures?.storage ? phoneInfo.mainFeatures.storage : "No date found"}</p>
+                            <h5 class="text-success">Sensors:</h5> 
+                            <p>${phoneInfo?.mainFeatures?.sensors ? phoneInfo.mainFeatures.sensors : "No date found"}</p>
+                            <h2 class="text-primary">Connectivity:</h2>
+                            <p>WLAN: ${phoneInfo?.others?.WLAN ? phoneInfo.others.WLAN : "No date found"}</p>
+                            <p>Bluetooth: ${phoneInfo?.others?.Bluetooth ? phoneInfo.others.Bluetooth : "No  date found"}</p>
+                            <p>GPS: ${phoneInfo?.others?.GPS ? phoneInfo.others.GPS : "No date found"}</p>
+                            <p>Radio: ${phoneInfo?.others?.Radio ? phoneInfo.others.Radio : "No date found"}</p>
+                            <p>USB: ${phoneInfo?.others?.USB ? phoneInfo.others.USB : "No date found"}</p>
+                            <p>NFC: ${phoneInfo?.others?.NFC ? phoneInfo.others.NFC : "No date found"}</p>
+                        </div>
+                        </div>
                     </div>
                 </div>
                 `;
